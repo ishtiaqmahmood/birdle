1ff5f76a274e2f52a9b5d9b616d9be9ed0527f1d
ishtiaqmahmood <ishtiaqmahmood45@gmail.com>
2026-08-12 15:44:10 +0600

    feat: Tile class added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'game.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,8 +13,33 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Tile('A', HitType.hit),
         ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget{
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white
+      }
       ),
     );
   }
